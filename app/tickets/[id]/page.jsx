8f22d4ced144d58979aab9d3c1ae725dc1dd3de2
nasser1410 <nasseraymansand@gmail.com>
2024-02-08 20:46:04 +0300

    commit after chapter 8

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,25 +1,43 @@
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true; // default val = true
+
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
 async function getTicket(id) {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
     next: {
       revalidate: 60,
     },
   });
-  console.log(res);
+
+  if (!res.ok) {
+    notFound();
+  }
+
   return res.json();
 }
 
-export default async function TicketsDetails({ params }) {
-  console.log(params.id);
+export default async function TicketDetails({ params }) {
+  // const id = params.id
   const ticket = await getTicket(params.id);
+
   return (
     <main>
       <nav>
-        <h2>Ticket Detail</h2>
+        <h2>Ticket Details</h2>
       </nav>
-
       <div className="card">
         <h3>{ticket.title}</h3>
-        <small>ticket created by {ticket.user_email}</small>
+        <small>Created by {ticket.user_email}</small>
         <p>{ticket.body}</p>
         <div className={`pill ${ticket.priority}`}>
           {ticket.priority} priority
